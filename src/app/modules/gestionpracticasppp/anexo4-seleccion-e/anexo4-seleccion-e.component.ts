8b13a9e81b6e0e8e39c82261f44bc039e952c537
OscarTendencias <tdscomplexivo.equipo4@tecazuay.edu.ec>
2022-06-13 19:21:48 -0500

    subida de documento anexo4 en asset

--- src/app/modules/gestionpracticasppp/anexo4-seleccion-e/anexo4-seleccion-e.component.ts
+++ src/app/modules/gestionpracticasppp/anexo4-seleccion-e/anexo4-seleccion-e.component.ts
@@ -12,6 +12,24 @@ import {Solicitudproyecto} from "../../../models/solicitudproyecto";
 import {Anexo2} from "../../../models/anexo2";
 import {DatePipe} from "@angular/common";
 import Docxtemplater from "docxtemplater";
+// @ts-ignore
+import PizZip from "pizzip";
+// @ts-ignore
+import PizZipUtils from "pizzip/utils/index.js";
+// @ts-ignore
+import { saveAs } from "file-saver";
+import {Materias} from "../../../models/materias";
+function loadFile(url:any, callback:any) {
+  PizZipUtils.getBinaryContent(url, callback);
+}
+function getBase64(file: any) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = error => reject(error);
+  });
+}
 
 @Component({
   selector: 'app-anexo4-seleccion-e',
@@ -61,8 +79,17 @@ export class Anexo4SeleccionEComponent implements OnInit {
     },1000)
   }
   ngOnInit(): void {
-    this.firstFormGroup = this._formBuilder.group({});
-    this.secondFormGroup = this._formBuilder.group({});
+    this.firstFormGroup = this._formBuilder.group({
+      fechaS:['',Validators.required],
+      nombreRepresentanteE:['', Validators.required],
+      cargo: ['',Validators.required],
+      empresa: ['',Validators.required]
+    });
+    this.secondFormGroup = this._formBuilder.group({
+      fechaE:['',Validators.required],
+      responsablePPP:['',Validators.required],
+      carrera:['',Validators.required],
+    });
     this.thirdFormGroup = this._formBuilder.group({
       docx:['',Validators.required]
     });
@@ -89,81 +116,81 @@ export class Anexo4SeleccionEComponent implements OnInit {
     console.log("datos"+this.anexo4)
   }
 
-  // generarDocumento(proyecto:Solicitudproyecto) {
-  //   console.log(this.obtenerDatosanexo4(proyecto))
-  //   var pipe:DatePipe = new DatePipe('en-US')
-  //   var anexo:Anexo2=this.obtenerDatosanexo4(proyecto);
-  //   loadFile("https://raw.githubusercontent.com/ComplexivoG2C2/CasoPractico2PPPFront/leo/src/assets/docs/Anexo2.docx", function(
-  //     // @ts-ignore
-  //     error,
-  //     // @ts-ignore
-  //     content
-  //   ) {
-  //     if (error) {
-  //       throw error;
-  //     }
-  //     const zip = new PizZip(content);
-  //     const doc = new Docxtemplater(zip, { paragraphLoop: true, linebreaks: true });
-  //
-  //
-  //     doc.setData({
-  //       // @ts-ignore
-  //       fecha:anexo.fecha,
-  //       siglas: anexo.siglasCarrera,
-  //       anio: anexo.anio,
-  //       nro: anexo.numeroConvocatoria,
-  //       ciclo: anexo.ciclo,
-  //       carrera: anexo.carrera,
-  //       empresa: anexo.empresa,
-  //       tb1: proyecto.actividadeslistProyectos,
-  //       tb2: proyecto.requisitoslistProyectos,
-  //       nombre_responsableppp: anexo.nombreResponsable,
-  //       fecha_max:pipe.transform(anexo.fechaMaxRecepcion,'dd/MM/yyyy'),
-  //     });
-  //     try {
-  //       // render the document (replace all occurences of {first_name} by John, {last_name} by Doe, ...)
-  //       doc.render();
-  //     } catch (error) {
-  //       // The error thrown here contains additional information when logged with JSON.stringify (it contains a properties object containing all suberrors).
-  //       // @ts-ignore
-  //       function replaceErrors(key, value) {
-  //         if (value instanceof Error) {
-  //           return Object.getOwnPropertyNames(value).reduce(function(
-  //               error,
-  //               key
-  //             ) {
-  //               // @ts-ignore
-  //               error[key] = value[key];
-  //               return error;
-  //             },
-  //             {});
-  //         }
-  //         return value;
-  //       }
-  //       //console.log(JSON.stringify({ error: error }, replaceErrors));
-  //       // @ts-ignore
-  //       if (error.properties && error.properties.errors instanceof Array) {
-  //         // @ts-ignore
-  //         const errorMessages = error.properties.errors
-  //           // @ts-ignore
-  //           .map(function(error) {
-  //             return error.properties.explanation;
-  //           })
-  //           .join("\n");
-  //         //console.log("errorMessages", errorMessages);
-  //         // errorMessages is a humanly readable message looking like this :
-  //         // 'The tag beginning with "foobar" is unopened'
-  //       }
-  //       throw error;
-  //     }
-  //     const out = doc.getZip().generate({
-  //       type: "blob",
-  //       mimeType:
-  //         "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
-  //     });
-  //     // Output the document using Data-URI
-  //     saveAs(out, "Anexo4 "+anexo.nombreResponsable+" Covocatoria Nª"+anexo.numeroConvocatoria+"de"+anexo.carrera+".docx");
-  //   });
-  // }
+  generarDocumento(proyecto:Solicitudproyecto) {
+    console.log(this.obtenerDatosanexo4(proyecto))
+    var pipe:DatePipe = new DatePipe('en-US')
+    var anexo:Anexo2=this.obtenerDatosanexo4(proyecto);
+    loadFile("https://raw.githubusercontent.com/ComplexivoG2C2/CasoPractico2PPPFront/oscar/src/assets/docs/Anexo4.docx", function(
+      // @ts-ignore
+      error,
+      // @ts-ignore
+      content
+    ) {
+      if (error) {
+        throw error;
+      }
+      const zip = new PizZip(content);
+      const doc = new Docxtemplater(zip, { paragraphLoop: true, linebreaks: true });
+
+
+      doc.setData({
+        // @ts-ignore
+        fecha:anexo.fecha,
+        siglas: anexo.siglasCarrera,
+        anio: anexo.anio,
+        nro: anexo.numeroConvocatoria,
+        ciclo: anexo.ciclo,
+        carrera: anexo.carrera,
+        empresa: anexo.empresa,
+        tb1: proyecto.actividadeslistProyectos,
+        tb2: proyecto.requisitoslistProyectos,
+        nombre_responsableppp: anexo.nombreResponsable,
+        fecha_max:pipe.transform(anexo.fechaMaxRecepcion,'dd/MM/yyyy'),
+      });
+      try {
+        // render the document (replace all occurences of {first_name} by John, {last_name} by Doe, ...)
+        doc.render();
+      } catch (error) {
+        // The error thrown here contains additional information when logged with JSON.stringify (it contains a properties object containing all suberrors).
+        // @ts-ignore
+        function replaceErrors(key, value) {
+          if (value instanceof Error) {
+            return Object.getOwnPropertyNames(value).reduce(function(
+                error,
+                key
+              ) {
+                // @ts-ignore
+                error[key] = value[key];
+                return error;
+              },
+              {});
+          }
+          return value;
+        }
+        //console.log(JSON.stringify({ error: error }, replaceErrors));
+        // @ts-ignore
+        if (error.properties && error.properties.errors instanceof Array) {
+          // @ts-ignore
+          const errorMessages = error.properties.errors
+            // @ts-ignore
+            .map(function(error) {
+              return error.properties.explanation;
+            })
+            .join("\n");
+          //console.log("errorMessages", errorMessages);
+          // errorMessages is a humanly readable message looking like this :
+          // 'The tag beginning with "foobar" is unopened'
+        }
+        throw error;
+      }
+      const out = doc.getZip().generate({
+        type: "blob",
+        mimeType:
+          "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+      });
+      // Output the document using Data-URI
+      saveAs(out, "Anexo4.docx");
+    });
+  }
 
 }
